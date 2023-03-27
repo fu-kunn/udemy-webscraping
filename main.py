39ff08375d8f191e6c368719deae17827c8f68ff
fu-kunn <sky53908@gmail.com>
2023-03-27 16:13:01 +0900

    [Add]受講生の数を取得

--- main.py
+++ main.py
@@ -5,4 +5,12 @@ import requests
 url = 'https://scraping-for-beginner.herokuapp.com/udemy'
 res = requests.get(url)
 soup = BeautifulSoup(res.text, 'html.parser')
-print(soup)
+
+# """
+# find_allで検索する癖をつける
+# ➡︎findだと最初の1件のみを取得するため
+# soup.find_all('p')
+# """
+
+n_subscriber = soup.find('p', {'class': 'subscribers'}).text
+print(n_subscriber)
