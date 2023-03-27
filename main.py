0dd796d0c870e4683f5e908d202b66b7e6ed80bd
fu-kunn <sky53908@gmail.com>
2023-03-27 16:59:38 +0900

    [Add] ECサイトから1つの商品価格を取得

--- main.py
+++ main.py
@@ -35,5 +35,6 @@ item_list = soup.find('ul', {'id': 'itemList'})
 items = item_list.find_all('li')
 item = items[0]
 title = item.find('p', {'class': 'items-grid_itemTitleText_31161d6a'}).text
+price = item.find('p', {'class': 'items-grid_price_31161d6a'}).text
 
-print(title)
+print(price)
