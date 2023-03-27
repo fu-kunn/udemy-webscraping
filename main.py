9599d0e9dd50fa337271e250ef00a548c5f725f4
fu-kunn <sky53908@gmail.com>
2023-03-27 15:56:10 +0900

    [Add]Webの情報を取得-BeautifulSoupで整形

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
+from bs4 import BeautifulSoup
+import requests
 
+
+url = 'https://scraping-for-beginner.herokuapp.com/udemy'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+print(soup)
